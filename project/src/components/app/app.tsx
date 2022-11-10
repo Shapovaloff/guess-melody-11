285fa7875add51cd589d28163511f453752f0339
Yury Shapovalov <yurij.shapovalov@in-aim.ru>
2022-11-10 13:22:22 +0300

    4.10. Удалит служебные маршруты.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,15 +1,13 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {HelmetProvider} from 'react-helmet-async';
-import { AppRoute, AuthorizationStatus } from '../../const';
-import ArtistQuestionScreen from '../../pages/artist-question-screen/artist-question-screen';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import AuthScreen from '../../pages/auth-screen/auth-screen';
 import GameOverScreen from '../../pages/game-over-screen/game-over-screen';
-import GenreQuestionScreen from '../../pages/genre-question-screen/genre-question-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
 import WinScreen from '../../pages/win-screen/win-screen';
 import PrivateRoute from '../private-route/private-route';
-import { QuestionArtist, QuestionGenre, Questions } from '../../types/question';
+import {Questions} from '../../types/question';
 import GameScreen from '../../pages/game-screen/game-screen';
 
 type AppScreenProps = {
@@ -18,7 +16,6 @@ type AppScreenProps = {
 }
 
 function App({errorsCount, questions}: AppScreenProps): JSX.Element {
-  const [firstQuestion, secondQuestion] = questions;
 
   return (
     <HelmetProvider>
@@ -27,28 +24,6 @@ function App({errorsCount, questions}: AppScreenProps): JSX.Element {
           <Route
             path={AppRoute.Root}
             element={<WelcomeScreen errorsCount={errorsCount} />}
-          />
-          <Route
-            path={AppRoute.DevArtist}
-            element={
-              <ArtistQuestionScreen
-                question={secondQuestion as QuestionArtist}
-                onAnswer={() => {
-                  throw new Error('Function \'onAnswer\' isn\'t implemented.');
-                }}
-              />
-            }
-          />
-          <Route
-            path={AppRoute.DevGenre}
-            element={
-              <GenreQuestionScreen
-                question={firstQuestion as QuestionGenre}
-                onAnswer={() => {
-                  throw new Error('Function \'onAnswer\' isn\'t implemented.');
-                }}
-              />
-            }
           />
           <Route
             path={AppRoute.Login}
